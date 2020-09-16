b64f72e0c3550649ce8536c1e08405b8e232e6d4
mac <shebah712@gmail.com>
2020-09-16 16:57:59 -0700

    practice scripts

---
+++ 713_list.py
@@ -0,0 +1,38 @@
+squad_713 = [
+    'Alice',
+    'Alpha',
+    'Anthony',
+    'Barent',
+    'Branden',
+    'Channee',
+    'Cristina',
+    'Cabassa',
+    'Elaine',
+    'Han',
+    'Irene',
+    'Joshua',
+    'Levin',
+    'Lizz',
+    'Margaret',
+    'Nicholas',
+    'Philip',
+    'Rohun',
+    'Sameh',
+    'Shane',
+    'Steven',
+    'Subrata',
+    'Tanner',
+    'Yoel',
+    'Adam',
+    'Pete',
+    'Rome',
+    'Taylor'
+]
+ga_file = open('general_assembly.txt', 'w')
+
+for i in range(len(squad_713)):
+    person = squad_713[i]
+
+    ga_file.write('{}\n'.format(person))
+
+ga_file.close()
